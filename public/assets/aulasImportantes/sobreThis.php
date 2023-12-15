7a5dd4966b0a3b0209ae6c89c1d6d301b02b3ae2
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2023-12-15 11:29:15 -0300

    Att: Aula sobre como funciona o $this

---
+++ public/assets/aulasImportantes/sobreThis.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\classes;
+
+class Crud
+{
+    //usando a propriedade $field dentro da função do método read
+    public $field = 'field teste';
+    public $table  = 'user';
+
+    public function read() 
+    {
+        //É o próprio objeto, faz referência ao objeto. Com o $this pode ser chamado qual objeto e qualquer método
+        var_dump($this->field);
+    }
+
+    public function delete() 
+    {
+        var_dump($this->read());
+
+    }
+
+}
