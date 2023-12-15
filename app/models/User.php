ff0074edad5feceea1cc8a71c32d2120fab18f5c
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2023-12-15 09:53:55 -0300

    Att: Aprendendo a usar o autoload do PHP, com composer e arquivo separado e namespace com classes obj

---
+++ app/models/User.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\models;
+
+class User
+{
+    public function teste()
+    {
+        return 'teste user';
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,10 +1,20 @@
 <?php 
 
-require '../app/classes/Login.php';
+//nome do diretório principal primeiro 'app', depois os subdiretórios 'classes' e as classes dos diretórios ex: 'Login'
+use app\models\User;
+use app\classes\Crud;
+use app\classes\Login;
+
+require '../vendor/autoload.php';
 
 //Chamando a classe e transformando ela em objeto
 $login = new Login;
+$crud = new Crud;
+$user = new User;
 
+echo helper();
+
+echo $user->teste();
 //desta forma são chamadas as propriedades, para assim dar um valor a elas
 $login->email = 'OPAAAA@gmail.com';
 $login->password = '12345'; 
@@ -12,14 +22,14 @@ $login->password = '12345';
 // var_dump($login);
 
 //Desta forma apenas é chamada a propriedade
-echo $login->email;
+// echo $login->email;
 
 //Assim é chamado o método
-echo $login->auth();
+// echo $login->auth();
 
 $user = ['name' => 'Guilherme', 'email' => 'guilhermedesouzamuller@hotmail.com'];
 
 $userObj = (object)$user;
-var_dump($userObj);
+// var_dump($userObj);
 
 
