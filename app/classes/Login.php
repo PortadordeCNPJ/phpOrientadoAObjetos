1203d442ebff1229435da13fc5a25ee53b3a07d1
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2023-12-18 11:59:37 -0300

    Att: atribuindo um tipo para as propiedades

--- app/classes/Login.php
+++ app/classes/Login.php
@@ -8,12 +8,12 @@ class Login
     //Dentro de uma Classe nós temos Metódos(Funções da classe) e Propriedades(Váriaveis de dentro da classe)
 
     //A propriedade e o metodo se complementam
-    public $email;
-    public $password;
+    // public string $email;
+    // public string $password;
 
-    public function auth()
+    public function auth(string $email, string $password)
     {
         //Esse é o método e o que ele vai fazer estará dentro destas chaves
-        return 'O meu email é ' . $this->email . ' e minha senha é ' . $this->password;
+        return 'O meu email é ' . $email . ' e minha senha é ' . $password;
     }
 }
